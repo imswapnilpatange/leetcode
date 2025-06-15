0eddac2cdf3f16b3eb4c8382d37934b8338d4fd5
imswapnilpatange <121827440+imswapnilpatange@users.noreply.github.com>
2025-06-15 13:23:20 +0530

    Time: 5 ms (18.16%), Space: 43.6 MB (73.48%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,23 +1,20 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int [] ans = new int [nums1.length];
-        Arrays.fill(ans, -1);
 
         for(int i = 0; i<nums1.length; i++){
-            for(int j = 0; j<nums2.length; j++){
-                if(nums1[i] == nums2[j]){
-                    jFor: for(int k = j+1; k<nums2.length; k++){
-                        if(nums1[i] < nums2[k]){
-                            ans[i] = nums2[k];
-                            break jFor;
-                        }
-                    }
-                }
-            }
-        }
+            int finalElement = -1;
+            int j = nums2.length-1;
 
+            while(j >= 0 && nums1[i] != nums2[j]){
+                if(nums1[i] < nums2[j]){
+                    finalElement = nums2[j];
+                }
+                j--;
+            }
+
+            ans[i] = finalElement;
+        }
         return ans;
     }
 }
