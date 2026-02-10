942fd8119ad62a5c8ba1ab3869982ac334e131b2
imswapnilpatange <121827440+imswapnilpatange@users.noreply.github.com>
2026-02-10 20:39:11 +0530

    Time: 102 ms (94.71%), Space: 47 MB (84.49%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
@@ -1,11 +1,14 @@
 class Solution {
     public int longestBalanced(int[] nums) {
         int n = nums.length;
-        int max = 0;
+        int longestBalancedSubarrayLength = 0;
         
         for(int i = 0; i < n; i++){
             Set<Integer> oddCount = new HashSet<>();
             Set<Integer> evenCount = new HashSet<>();
+
+            // Optimization: If remainingElementsCount is less than maxLength, then we don't need to iterate those.
+            if (longestBalancedSubarrayLength >= n - i) break;
 
             for(int j = i; j < n; j++){
                 if(nums[j] % 2 == 1){
@@ -15,9 +18,9 @@ class Solution {
                 }
 
                 if(oddCount.size() == evenCount.size())
-                max = Math.max(max, j - i + 1);
+                longestBalancedSubarrayLength = Math.max(longestBalancedSubarrayLength, j - i + 1);
             }
         }
-        return max;       
+        return longestBalancedSubarrayLength;       
     }
 }
