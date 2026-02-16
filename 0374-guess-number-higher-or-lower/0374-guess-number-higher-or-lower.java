0814380b97b31dc9c008e50a694b99bd2af89d1a
imswapnilpatange <121827440+imswapnilpatange@users.noreply.github.com>
2026-02-17 01:09:08 +0530

    Time: 0 ms (100%), Space: 42.2 MB (32.73%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
@@ -14,8 +14,9 @@ public class Solution extends GuessGame {
         int low = 1, high = n;
         while(low <= high){
             int mid = low + (high - low) / 2;
-            if(guess(mid) == 0) return mid;
-            else if(guess(mid) == 1) low = mid + 1; // Search higher
+            int res = guess(mid);
+            if(res == 0) return mid;
+            else if(res == 1) low = mid + 1; // Search higher
             else high = mid - 1; //Search lower
         }
         return -1;
