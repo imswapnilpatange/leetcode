6ae16bda3e610a1ab682362746e97d534c4aff78
imswapnilpatange <121827440+imswapnilpatange@users.noreply.github.com>
2026-02-21 23:47:43 +0530

    Time: 2 ms (97.34%), Space: 44.8 MB (48.53%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,12 +1,29 @@
+// Optimum - O(n)
 class Solution {
     public boolean checkIfExist(int[] arr) {
-
-        for(int i = 0; i < arr.length; i++){
-            for(int j = i + 1; j < arr.length; j++){
-                if(arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i])
-                    return true;
-            }
+        Set<Integer> seen = new HashSet<>();
+        for(int x: arr){
+            if(seen.contains(2 * x) || (x % 2 == 0 && seen.contains(x / 2)))
+                return true;
+            
+            seen.add(x);
         }
         return false;
     }
 }
+
+// Brute Force - O(n^2)
+/**
+    class Solution {
+        public boolean checkIfExist(int[] arr) {
+
+            for(int i = 0; i < arr.length; i++){
+                for(int j = i + 1; j < arr.length; j++){
+                    if(arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i])
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+ */
