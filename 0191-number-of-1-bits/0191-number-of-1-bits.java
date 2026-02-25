9f34fc8603d08a7bc1bb5b063a78b669f0385aa7
imswapnilpatange <121827440+imswapnilpatange@users.noreply.github.com>
2026-02-26 02:30:10 +0530

    Time: 0 ms (100%), Space: 42.7 MB (26.64%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,5 +1,23 @@
 class Solution {
     public int hammingWeight(int n) {
-        return Integer.bitCount(n);
+        if(n == 0) return 0;        // Edge Case
+
+        int count = 0;
+        while(n != 0){              // For all iteration, remove rightmost bit
+            n = n & (n - 1);        // Core Operation: removes lowest set bit
+            count++;                // Increment count for every removed bit
+        }
+
+        return count;
     }
 }
+
+/**
+// Another Option: Time Complexity - O(1); Space Complexity - O(1)
+class Solution {
+    public int hammingWeight(int n) {
+        return Integer.bitCount(n);
+        
+    }
+}
+*/
