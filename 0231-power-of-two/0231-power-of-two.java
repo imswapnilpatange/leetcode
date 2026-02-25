fe4c0748be207c00718adc544c227d2953703300
imswapnilpatange <121827440+imswapnilpatange@users.noreply.github.com>
2026-02-26 02:53:53 +0530

    Time: 1 ms (95.95%), Space: 42.7 MB (35.66%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n <= 0) return false;            //If n = 2^k exists only for positive numbers
+        if(n <= 0) return false;            // If n = 2^k exists only for positive numbers
 
-        return (n & (n - 1)) == 0;
+        return (n & (n - 1)) == 0;          // Condition to check if n = 2^k
     }
 }
