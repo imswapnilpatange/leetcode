b85228a12adb8a7e2215f4ea4f3f1404748db3c3
imswapnilpatange <121827440+imswapnilpatange@users.noreply.github.com>
2026-03-31 04:45:33 +0530

    Time: 1 ms (90.62%), Space: 42.6 MB (63.81%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,7 +1,7 @@
 class Solution {
       public boolean isPowerOfFour(int n) {
         if (n <= 0) return false;
-        if ((n & (n - 1)) != 0) return false;                       // Check if power of 2
+        if ((n & (n - 1)) != 0) return false;          // Check if power of 2
         if ((n & 0x55555555) == 0) return false;       // Check if set bit is at even position
          return true;
       }
